767ddc94959fab8c648694f6f08d3d22f4b9923d
ahmed maraey <ahmedmohamedmar3y2017@gmail.com>
2026-03-24 23:03:48 +0200

    -- add book using ref

--- src/components/AddForm/addForm.jsx
+++ src/components/AddForm/addForm.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { use, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { insertBook } from "../../store/bookSlice";
 
 const AddForm = () => {
+  // 1 - useRef solution
+  const title = useRef(null);
+  const price = useRef(null);
+  const desc = useRef(null);
+
+  const dispatch = useDispatch();
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const bookData = {
+      title: title.current.value,
+      price: price.current.value,
+      description: desc.current.value,
+    };
+
+    dispatch(insertBook(bookData));
+  };
+
   return (
     <div class="flex justify-center items-center bg-gray-100 p-6">
-      <form class="bg-white p-6 rounded-2xl shadow-md w-full max-w-md space-y-4">
+      <form
+        class="bg-white p-6 rounded-2xl shadow-md w-full max-w-md space-y-4"
+        onSubmit={handleSubmitForm}
+      >
         <h2 class="text-2xl font-bold text-center">Add Book</h2>
 
         <div>
           <label class="block mb-1 font-medium">Title</label>
           <input
+            ref={title}
             type="text"
             name="title"
             placeholder="The book2"
@@ -19,6 +43,7 @@ const AddForm = () => {
         <div>
           <label class="block mb-1 font-medium">Description</label>
           <textarea
+            ref={desc}
             name="description"
             placeholder="A classic American novel"
             class="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -28,6 +53,7 @@ const AddForm = () => {
         <div>
           <label class="block mb-1 font-medium">Price</label>
           <input
+            ref={price}
             type="number"
             name="price"
             placeholder="30"
--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -13,6 +13,25 @@ export const getBooks = createAsyncThunk(
   },
 );
 
+export const insertBook = createAsyncThunk(
+  "books/insertBook",
+  async (bookData, { rejectWithValue }) => {
+    try {
+      const response = await fetch("http://localhost:3005/book", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bookData),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
 const bookSlice = createSlice({
   name: "books",
   initialState: {
@@ -22,6 +41,7 @@ const bookSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
+      // get books
       .addCase(getBooks.pending, (state, action) => {
         state.isLoading = true;
         state.error = null;
@@ -31,6 +51,21 @@ const bookSlice = createSlice({
         state.books = action.payload;
       })
       .addCase(getBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      // insert books
+      .addCase(insertBook.pending, (state, action) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(insertBook.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.books.push(action.payload); // push to state books
+        // or insert + previous state value
+        // state.books = [...state.books, action.payload];
+      })
+      .addCase(insertBook.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
       });
